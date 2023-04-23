8638c824d08358b9840ab403dfea4292d26e2d39
MainaFiona <mainawanguifiona@gmail.com>
2023-04-23 23:31:36 +0000

    function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -2,6 +2,32 @@
 #include <stdio.h>
 
 /**
+ * *get_op_func - function that selects the correct function to perform
+ * @s: takes format specifier
+ *
+ * Return: Always 0.
+ */
+int (*get_op_func(const char *s))(va_list)
+{
+	op_t  ops[] = {
+		{"c", print_a_char},
+		{"s", print_a_str},
+		{"%", print_a_pct},
+		{NULL, NULL}
+	};
+	unsigned int i = 0;
+
+	while (ops[i].c)
+	{
+		if (*s == *(ops[i].c))
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+/**
  * _printf - function that produces output according to a format
  * @format: takes format specifier
  *
