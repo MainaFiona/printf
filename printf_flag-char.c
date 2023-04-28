7cd4039242ce103cbe6da9b53ee4571dcecbd875
arnoldmn <arnoldnzibuk@gmail.com>
2023-04-28 23:58:26 +0000

    few fixes

--- printf_flag-char.c
+++ printf_flag-char.c
@@ -1,101 +1,4 @@
 #include "main.h"
-
-/**
- * print_hex_lower - Print hexadecimal number in lowercase
- * @vl: va_list
- * Return: int
- */
-
-int print_hex_lower(va_list vl)
-{
-	unsigned int num = va_arg(vl, unsigned int);
-	int digits = 0, i;
-	char hex[16] = "0123456789abcdef";
-
-	if (num == 0)
-		return (_putchar('0'));
-
-	while (num != 0)
-	{
-		digits++;
-		num /= 16;
-	}
-
-	for (i = digits - 1; i >= 0; i--)
-		_putchar(hex[(va_arg(vl, unsigned int) >> (i * 4)) & 0xF]);
-
-	return (digits);
-}
-
-/**
- * print_hex_upper - Print hexadecimal number in uppercase
- * @vl: va_list
- * Return: int
- */
-
-int print_hex_upper(va_list vl)
-{
-	unsigned int num = va_arg(vl, unsigned int);
-	int digits = 0, i;
-	char hex[16] = "0123456789ABCDEF";
-
-	if (num == 0)
-		return (_putchar('0'));
-
-	while (num != 0)
-	{
-		digits++;
-		num /= 16;
-	}
-
-	for (i = digits - 1; i >= 0; i--)
-		_putchar(hex[(va_arg(vl, unsigned int) >> (i * 4)) & 0xF]);
-
-	return (digits);
-}
-
-/**
- * print_hex - Print hexadecimal number
- * @c: unsigned long int
- * @cap: int
- * Return: int
- */
-
-int print_hex(unsigned long int c, int cap)
-{
-	int digits = 0, i;
-	char hex[16];
-
-	if (cap)
-	{
-		for (i = 0; i < 10; i++)
-			hex[i] = i + '0';
-		for (; i < 16; i++)
-			hex[i] = i - 10 + 'A';
-	}
-	else
-	{
-		for (i = 0; i < 10; i++)
-			hex[i] = i + '0';
-		for (; i < 16; i++)
-			hex[i] = i - 10 + 'a';
-	}
-
-	if (c == 0)
-		return (_putchar('0'));
-
-	while (c != 0)
-	{
-		digits++;
-		c /= 16;
-	}
-
-	for (i = digits - 1; i >= 0; i--)
-		_putchar(hex[(c >> (i * 4)) & 0xF]);
-
-	return (digits);
-}
-
 /**
  * printf_flag - function that handles specifier
  * @args: formatted string to print arguments
@@ -104,43 +7,62 @@ int print_hex(unsigned long int c, int cap)
  */
 int printf_flag(va_list args)
 {
-	int i = 0, lgt = 0;
-	char *s = va_arg(args, char *);
-	unsigned int h;
-	char nil[] = "(null)";
+	const char *format = va_arg(args, const char *);
 
-	if (!s)
+	while (*format != '\0')
 	{
-		while (nil[i])
+		if (*format == '%')
 		{
-			_putchar(nil[i]);
-			i++;
-		}
-		return (i);
-	}
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] < 32 || s[i] >= 127)
-		{
-			_putchar(92);
-			_putchar(120);
-			lgt = lgt + 2;
-			h = s[i];
-			if (h < 16)
+			format++;
+			int leftAlign = 0;
+			if (*format == '-')
 			{
-				_putchar(48);
-				lgt++;
+				leftAlign = 1;
+				format++;
+			}
+
+			if (*format == 'd')
+			{
+				int value = va_arg(args, int);
+
+				if (leftAlign)
+				{
+					_putchar('-');
+					_putchar('0' + value);
+				}
+				else
+				{
+					_putchar('0' + value);
+				}
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				if (leftAlign)
+				{
+					while (*str != '\0')
+					{
+						_putchar(*str);
+						str++;
+					}
+				}
+				else
+				{
+					while (*str != '\0')
+					{
+						_putchar(*str);
+						str++;
+					}
+				}
 			}
 		}
 		else
 		{
-			_putchar(s[i]);
-			lgt++;
+			_putchar(*format);
 		}
-		i++;
+
+		format++;
 	}
 
-	return (lgt);
 }
